ecf2c3f258f0549df31680b6006bf745a67a1ce4
unknown <will.ediger@gmail.com>
2019-07-31 17:18:41 -0500

    added 'dotenv', changed port to read from env

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
+const port = process.env.PORT;
+
 const server = require("./server.js");
 
-server.listen(4000, () => {
-  console.log("\n* Server Running on http://localhost:4000 *\n");
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
